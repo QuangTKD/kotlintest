a556a0dd00102d3be284cbadbb2062189c166a8c
Nguyen Quoc Tuan <tuannqse03247@tuannqse03247.local>
2018-08-03 21:58:09 +0700

    Add Student Realm Table

---
+++ app/src/main/java/com/quangtkd/kotlintest/model/Student.kt
@@ -0,0 +1,20 @@
+package com.quangtkd.kotlintest.model
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import io.realm.annotations.RealmClass
+import io.realm.annotations.Required
+import java.util.*
+
+@RealmClass
+open class Student(
+        @PrimaryKey
+        @Required
+        var id: String = UUID.randomUUID().toString(),
+
+        @Required
+        var name: String = "",
+
+        @Required
+        var age: Int = 0
+
+) : RealmObject()
